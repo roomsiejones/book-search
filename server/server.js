b50cdf9fa9bd61128f73ce6d571142309429f329
Roomsie Jones <seangravito@gmail.com>
2021-08-24 19:12:21 -0500

    added schemas folder and the 3 files needed

--- server/server.js
+++ server/server.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+// add these folders and files below.
+const {typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth.js');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers,
+  context: authMiddleware,
+})
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+server.applyMiddleware({ app });
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+// needs to be a get route
 app.use(routes);
 
 db.once('open', () => {
